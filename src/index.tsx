ca2cdbee9212ce338bd3eba7ca50f6adb8d06b34
DamithaAmarakoon <damithamewan201@gmail.com>
2022-10-02 08:44:15 +0530

    tabWidth changed to 2

--- src/index.tsx
+++ src/index.tsx
@@ -9,14 +9,14 @@ import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -4,16 +4,16 @@ import { BankAction } from '../action-types/bank';
 const initialState = 0;
 
 const reducer = (state: number = initialState, action: BankAction) => {
-  switch (action.type) {
-    case BankActionType.DEPOST:
-      return state + action.payload;
-    case BankActionType.WITHDRAW:
-      return state === 0 ? state : state - action.payload;
-    case BankActionType.BANKRUPT:
-      return 0;
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case BankActionType.DEPOST:
+            return state + action.payload;
+        case BankActionType.WITHDRAW:
+            return state === 0 ? state : state - action.payload;
+        case BankActionType.BANKRUPT:
+            return 0;
+        default:
+            return state;
+    }
 };
 
 export default reducer;
