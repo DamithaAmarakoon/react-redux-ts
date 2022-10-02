8eddbd10ab135e445e7c9c84984e928c73617630
DamithaAmarakoon <damithamewan201@gmail.com>
2022-10-02 08:51:37 +0530

    husky - success

--- src/state/actions/bank/index.ts
+++ src/state/actions/bank/index.ts
@@ -3,20 +3,20 @@ import { BankActionType } from './types';
 import { BankAction } from '../../action-types';
 
 export const depositMoney =
-    (amount: number) => (dispatch: Dispatch<BankAction>) =>
-        dispatch({
-            type: BankActionType.DEPOST,
-            payload: amount
-        });
+  (amount: number) => (dispatch: Dispatch<BankAction>) =>
+    dispatch({
+      type: BankActionType.DEPOST,
+      payload: amount
+    });
 
 export const withdrawMoney =
-    (amount: number) => (dispatch: Dispatch<BankAction>) =>
-        dispatch({
-            type: BankActionType.WITHDRAW,
-            payload: amount
-        });
+  (amount: number) => (dispatch: Dispatch<BankAction>) =>
+    dispatch({
+      type: BankActionType.WITHDRAW,
+      payload: amount
+    });
 
 export const bankrupt = () => (dispatch: Dispatch<BankAction>) =>
-    dispatch({
-        type: BankActionType.BANKRUPT
-    });
+  dispatch({
+    type: BankActionType.BANKRUPT
+  });
